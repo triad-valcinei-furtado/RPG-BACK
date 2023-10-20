a9135b5c5d9659968c3c2e16f4983861d29c9b1b
triad-valcinei-furtado <valcinei.furtado@triadpesquisa.com.br>
2023-10-20 18:23:12 -0300

    inciando o socket

--- index.ts
+++ index.ts
@@ -1,16 +1,42 @@
-import express, { Express, Request, Response, Application } from "express";
+import express, { Request, Response, Application } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import { Server } from "socket.io";
+import {
+  ClientToServerEvents,
+  InterServerEvents,
+  ServerToClientEvents,
+  SocketData,
+} from "./types";
+
+const app: Application = express();
+const http = require("http").Server(app);
+
+const io = new Server<
+  ClientToServerEvents,
+  ServerToClientEvents,
+  InterServerEvents,
+  SocketData
+>(http, {
+  cors: {
+    origin: "*",
+  },
+});
 
 //For env File
 dotenv.config();
 
-const app: Application = express();
-const port = process.env.PORT || 9000;
+const PORT = process.env.PORT || 9000;
+
+app.use(cors());
+app.use(express.json());
+
+io.on("connection", (socket) => {});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Express & TypeScript Server");
 });
 
-app.listen(port, () => {
-  console.log(`Server is Fire at http://localhost:${port}`);
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
